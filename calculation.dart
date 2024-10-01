83c9ffaac73902bae9dd1af4f14ba925a45b9e86
Thien Nguyen <thien.nguyen@pratek.vn>
2024-10-01 09:28:44 +0700

    test

---
+++ calculation.dart
@@ -0,0 +1,14 @@
+void main() {
+  var num1 = 10;
+  var num2 = 3;
+
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div = num1 / num2;
+
+  print("$num1 + $num2 = $sum");
+  print("$num1 - $num2 = $diff");
+  print("$num1 * $num2 = $mul");
+  print("$num1 / $num2 = $div");
+}
--- helloword.dart
+++ helloword.dart
@@ -1,3 +1,10 @@
 void main() {
+  //print statement
   print("Hello world!");
+
+  //declare variables
+  var firstname = "Thien";
+  var lastname = "Nguyen";
+  print("My name is " + firstname + lastname);
+  print("My name is: $lastname $firstname");
 }
